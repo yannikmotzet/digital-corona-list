2ad1d08dee8be5545d8910adb9a1c22290f1826e
yannikmotzet <yannik.motzet@outlook.com>
2021-01-08 18:30:37 +0100

    app: changed alert dialog to material alert dialog

--- app/src/main/java/com/example/testbeacon/MainActivity.java
+++ app/src/main/java/com/example/testbeacon/MainActivity.java
@@ -163,33 +163,33 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer, R
     private void verifyBluetooth() {
         try {
             if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Bluetooth not enabled");
-                builder.setMessage("Please enable bluetooth in settings and restart this application.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                new MaterialAlertDialogBuilder(this)
+                        .setTitle("Bluetooth not enabled")
+                        .setMessage("Please enable bluetooth in settings and restart this application.")
+                        .setPositiveButton(android.R.string.ok, null)
+                        .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
                         //finish();
                         //System.exit(0);
                     }
-                });
-                builder.show();
+                })
+                        .show();
             }
         }
         catch (RuntimeException e) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Bluetooth LE not available");
-            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("Bluetooth LE not available")
+                    .setMessage("Sorry, this device does not support Bluetooth LE.")
+                    .setPositiveButton(android.R.string.ok, null)
+                    .setOnDismissListener(new DialogInterface.OnDismissListener() {
                 @Override
                 public void onDismiss(DialogInterface dialog) {
                     //finish();
                     //System.exit(0);
                 }
-            });
-            builder.show();
+            })
+                .show();
         }
     }
 
